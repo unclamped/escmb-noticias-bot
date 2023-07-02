bc0653da5374070a34925c6eb33ad6412b4c9816
unclamped <clear6860@tutanota.com>
2023-07-02 11:04:03 -0300

    initial refactor

--- index.ts
+++ index.ts
@@ -1,68 +1,75 @@
-import * as cheerio from 'cheerio';
 import axios from 'axios';
-import * as fs from 'fs';
-import { Client, LocalAuth, MessageMedia, MessageAck, Message } from 'whatsapp-web.js';
+import * as sqlite3 from 'sqlite3';
+import { Client, LocalAuth, MessageMedia, MessageAck } from 'whatsapp-web.js';
+import * as cheerio from 'cheerio';
 import * as qrcode from 'qrcode-terminal';
+
+const db = new sqlite3.Database('posts.db');
+db.run(`CREATE TABLE IF NOT EXISTS posts (url TEXT PRIMARY KEY)`);
 
 (async () => {
   try {
-    const respuestaMain = await axios.get('https://mb.unc.edu.ar/');
-    const main$ = cheerio.load(respuestaMain.data);
-
-    const articulos = main$('article.post.fusion-column.column.col.col-lg-4.col-md-4.col-sm-4');
-
-    const articlePromises = articulos.toArray().map(async (element) => {
-      const tiempo = main$(element).find('span.updated').text();
-
-      if (!fs.existsSync('ultimo_articulo.txt')) {
-        fs.writeFileSync('ultimo_articulo.txt', '');
+    const client = new Client({
+      authStrategy: new LocalAuth(),
+      puppeteer: {
+        headless: true
       }
+    });
 
-      let fechaArticulo = fs.readFileSync('ultimo_articulo.txt', 'utf8').trim();
+    let pendiente: string;
+    let enviado: boolean = false;
 
-      // Check if the current article is newer than the last recorded article
-      if (tiempo > fechaArticulo) {
-        const url = main$(element).find('a.hover-type-none').attr('href');
-        const titulo = main$(element).find('h4.entry-title').text();
-        const previewTexto = main$(element).find('p:not(.meta)').first().text();
-        const imagen = main$(element).find('img.attachment-full.size-full').attr('src');
+    client.on('qr', qr => {
+      qrcode.generate(qr, {small: true});
+    });
 
-        const respuestaArticulo = (await axios.get(url!)).data; // usando ! porque ningun post en la web no va a tener un link
-        const articulo$ = cheerio.load(respuestaArticulo);
+    client.on('ready', async () => {
+      const respuestaMain = await axios.get('https://mb.unc.edu.ar/');
+      const main$ = cheerio.load(respuestaMain.data);
+      const articulos = main$('article.post.fusion-column.column.col.col-lg-4.col-md-4.col-sm-4');
 
-        // const primerParrafo = articulo$(element).find('div.fusion-text.fusion-text-1 p').first().text().trim(); TODO: Copiar parrafo entero
-        // console.log('Primer Parrafo:', primerParrafo);
+      for (let i = articulos.length - 1; i >= 0; i--) {
+        const articulo = articulos.eq(i);
 
-        const client = new Client({
-          authStrategy: new LocalAuth(),
-          puppeteer: {
-            headless: true
-          }
-        });
+        const url = main$(articulo).find('a.hover-type-none').attr('href') as string;
 
-        let message: Promise<Message>; 
+        const getUrlExistsPromise = (url: string) => new Promise((resolve, reject) => 
+          db.get('SELECT EXISTS(SELECT 1 FROM posts WHERE url = ?) AS urlExiste', [url],
+            (error, row: any) => error ? reject(error) : resolve(row.urlExiste === 1))
+            );
 
-        client.on('qr', qr => {
-          qrcode.generate(qr, {small: true});
-        });
+        const yaExiste = await getUrlExistsPromise(url);
+        console.log(yaExiste)
+        
+        if (yaExiste) {
+          return;
+        }
 
-        client.on('ready', async () => {
-          const media = await MessageMedia.fromUrl(imagen!);
-          message = client.sendMessage('xxx@g.us', media, {caption: `${titulo}\n\n${previewTexto}\n\n${url}`})
-        });
+        const titulo = main$(articulo).find('h4.entry-title').text();
+        const previewTexto = main$(articulo).find('p:not(.meta)').first().text();
+        const imagen = main$(articulo).find('img.attachment-full.size-full').attr('src');
 
-        client.on('message_ack', async (ackMessage, ack) => {
-          if (ack === MessageAck.ACK_DEVICE && ackMessage.id.id === (await message).id.id) {
-            fs.writeFileSync('ultimo_articulo.txt', tiempo);
-            client.destroy();
-            process.exit()
-          }
-        });
-
-        client.initialize();
+        const media = await MessageMedia.fromUrl(imagen!);
+        let mensaje = client.sendMessage('120363158664052984@g.us', media, {caption: `${titulo}\n\n${previewTexto}\n\n${url}`});
+        pendiente = (await mensaje).id.id;
+        while (!enviado) {}
+        
+        db.run('INSERT INTO posts (url) VALUES (?)', [url])
+        enviado = false;
       }
-    })
-  } catch (error) {
-    console.error(error);
+    });
+
+    client.on('message_ack', async (ackMessage, ack) => {
+      console.log(ack)
+      if (ack === MessageAck.ACK_DEVICE && ackMessage.id.id === pendiente) {
+        enviado = true;
+      }
+    });
+
+    client.initialize();
+  } catch (err) {
+    console.error(err);
+    db.close();
+    process.exit();
   }
 })();
